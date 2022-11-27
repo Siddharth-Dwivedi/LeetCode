83274bfcff96dc293709f505e45e089ba429c3ae
Siddharth Dwivedi <82795203+Siddharth-Dwivedi@users.noreply.github.com>
2022-11-27 19:05:13 +0530

    Time: 75 ms (58.69%), Space: 6.6 MB (62.89%) - LeetHub

--- 2380-time-needed-to-rearrange-a-binary-string/2380-time-needed-to-rearrange-a-binary-string.cpp
+++ 2380-time-needed-to-rearrange-a-binary-string/2380-time-needed-to-rearrange-a-binary-string.cpp
@@ -1,43 +1,20 @@
 class Solution {
 public:
-int secondsToRemoveOccurrences(string s) {
-    int seconds = 0, changed = true;
-    while(changed) {
-        changed = false;
-        for (int i = 0; i < s.size() - 1; ++i) {
-            if (s[i] == '0' && s[i + 1] == '1') {
-                swap(s[i], s[i + 1]);
-                ++i;
-                changed = true;
+    int secondsToRemoveOccurrences(string s) {
+        int flag =0;
+        int count=0;
+        while(flag!=1){
+            flag=1;
+            for(int i=0;i<s.size()-1;i++){
+                if(s[i]=='0' && s[i+1]=='1'){
+                    swap(s[i],s[i+1]);
+                    i++;
+                    flag=0;
+                }
             }
+            if(flag==0)
+            count++;
         }
-        seconds += changed;
+        return count;
     }
-    return seconds;
-}
- };
-
-
-// class Solution {
-// public:
-//     int secondsToRemoveOccurrences(string s) {
-//         int i=0,j=s.size()-1;
-//         int count=0;
-//         while(i<j){
-//             if(s[i]!='0'){
-//                 i++;
-//             }
-//             if(s[j]!='1'){
-//                 j--;
-//             }
-//             if(s[i]=='0' && s[j]=='1'){
-//                 swap(s[i],s[j]);
-//                 count++;
-//                 i++;
-//                 j--;
-                
-//             }
-//         }
-//         return count;
-//     }
-// };
+};
