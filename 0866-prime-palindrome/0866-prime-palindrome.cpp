a0a539be6d21aab4cbcc98b327f300c76d5bb2fb
Siddharth Dwivedi <82795203+Siddharth-Dwivedi@users.noreply.github.com>
2022-12-01 22:37:38 +0530

    Time: 1759 ms (7.28%), Space: 6 MB (48.34%) - LeetHub

---
+++ 0866-prime-palindrome/0866-prime-palindrome.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    bool checkPalindrome(int n){
+        int r=n;
+        int sum=0;
+        int i=0;
+
+        while(n>0){
+            int k = n%10;
+            sum=sum*10 +k;
+            n = n/10;
+        }
+        return sum==r;
+    }
+    
+    bool checkprime(int n){
+        if(n<=1){
+            return false;
+        }
+        else{
+            for(int i=2;i<n;i++){
+                if(n%i==0){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    
+    int smallestNo(int n){
+        if(checkPalindrome(n)){
+            if(checkprime(n)){
+                return n;
+            }
+        }
+        return smallestNo(n+1);
+    }
+    
+    int primePalindrome(int n) {
+         if (1e7 <= n && n <= 1e8)
+        return 100030001;
+        int a = smallestNo(n);
+        return a;
+    }
+
+};
