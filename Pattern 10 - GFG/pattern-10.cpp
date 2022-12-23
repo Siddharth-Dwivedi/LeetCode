55ca5741fecaf60f57e8262a8bc1ad433e96028a
Siddharth Dwivedi <82795203+Siddharth-Dwivedi@users.noreply.github.com>
2022-12-23 15:53:22 +0530

    Added solution - LeetHub

---
+++ Pattern 10 - GFG/pattern-10.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+//Back-end complete function Template for C++
+class Solution{
+public:
+    void printTriangle(int n) {
+        // code here
+        for(int i=0;i<2*n-1;i++){
+            if(i<n){
+                for(int j=0;j<n;j++){
+                    if(j<=i){
+                        cout<<"* ";
+                    }
+                    // else
+                    //     cout<<" ";
+                }
+            }
+            else{
+                for(int j=0;j<n;j++){
+                    if(j<2*n-1-i){
+                        cout<<"* ";
+                    }
+                    // else{
+                    //     cout<<" ";
+                    // }
+                }
+            }
+            cout<<endl;   
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        Solution ob;
+        ob.printTriangle(n);
+    }
+    return 0;
+}
+// } Driver Code Ends
